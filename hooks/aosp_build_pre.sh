8e1bdd771a31d4436c10da7f7101adce5053eba0
ef <contact@vcranz.de>
2021-03-08 12:01:49 +0100

    custom patches

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -12,6 +12,18 @@ community_patches_dir="${ROOT_DIR}/community_patches"
 rm -rf "${community_patches_dir}"
 git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00002-round-icon.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00003-enable-volte-wifi-calling.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00004-use-cloudflare-dns.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00007-set-seedvault-as-dftl-bkp-provider.patch"
+
+# more patches
+community_patches_dir="${ROOT_DIR}/community_patches/custom"
+git clone https://github.com/coxtor/ros-custom-config-repo-bramble.git "$community_patches_dir"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00005-allow-unifiednlp-location-provider.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00010-backup-any-application.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/10001-move-statusbar-clock-back-to-rhs.patch"
+
 
 # apply custom hosts file
 custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
